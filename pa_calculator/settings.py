2ea880eb6818467c6efdc51a2c7aa54fbe2ecce5
Mikhael <mikhacordeiro@gmail.com>
2024-11-22 11:30:59 -0300

    Remove dj_database_url e simplifica configuração

--- pa_calculator/settings.py
+++ pa_calculator/settings.py
@@ -78,10 +78,12 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
